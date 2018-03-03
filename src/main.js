0a9be46c669d0c4e3e9f9203194fa4c4ab8071d2
tchaguitos <tchaguitos@gmail.com>
2018-03-02 22:22:47 -0300

    add main tests

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 module.exports = {
-  sum: () => {},
-  sub: () => {},
-  mult: () => {},
-  div: () => {},
+  sum: (num1, num2) => num1 + num2,
+  sub: (num1, num2) => num1 - num2,
+  mult: (num1, num2) => num1 * num2,
+  div: (num1, num2) => {
+    if (num2 === 0) return 'no possible, asshole!';
+
+    return num1 / num2;
+  },
 };
--- tests/main.spec.js
+++ tests/main.spec.js
@@ -32,5 +32,33 @@ describe('Calc', () => {
       expect(calc.div).to.be.a('function');
     });
   });
+
+  describe('Sum', () => {
+    it('should return 4 when `calc.sum(2, 2)`', () => {
+      expect(calc.sum(2, 2)).to.be.equal(4);
+    });
+  });
+
+  describe('Sub', () => {
+    it('should return 2 when `calc.sub(7, 5)`', () => {
+      expect(calc.sub(7, 5)).to.be.equal(2);
+    });
+  });
+
+  describe('Mult', () => {
+    it('should return 6 when `calc.mult(2, 3)`', () => {
+      expect(calc.mult(2, 3)).to.be.equal(6);
+    });
+  });
+
+  describe('Div', () => {
+    it('should return 5 when `calc.div(25, 5)`', () => {
+      expect(calc.div(25, 5)).to.be.equal(5);
+    });
+
+    it('should return `no possible, asshole!` when divide by 0', () => {
+      expect(calc.div(4, 0)).to.be.equal('no possible, asshole!');
+    });
+  });
 });
 
