59484b926e4e38ae2d85634040fab132ba40b5c8
tchaguitos <tchaguitos@gmail.com>
2018-03-03 02:20:39 -0300

    add fizzbuzz method

---
+++ src/fizzbuzz.js
@@ -0,0 +1,10 @@
+const Fizzbuzz = (number) => {
+  if (number % 3 === 0 && number % 5 === 0) return 'FizzBuzz';
+  if (number % 3 === 0) return 'Fizz';
+  if (number % 5 === 0) return 'Buzz';
+
+  return number;
+};
+
+export { Fizzbuzz };
+
