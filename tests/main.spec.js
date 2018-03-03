baae2c2625891297a3262fc1149e065d9f935f70
tchaguitos <tchaguitos@gmail.com>
2018-03-03 01:29:59 -0300

    add main spec

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,63 +1,59 @@
-const chai = require('chai');
-const { expect } = require('chai');
-const { describe, it } = require('mocha');
-const dirtyChai = require('dirty-chai');
-const calc = require('../src/main');
+import { use, expect } from 'chai';
+import { describe, it } from 'mocha';
 
-chai.use(dirtyChai);
+import dirtyChai from 'dirty-chai';
+import { sum, sub, mult, div } from '../src/main';
+
+use(dirtyChai);
 
 describe('Calc', () => {
   describe('Smoke tests', () => {
-    it('should exit `calc` lib', () => {
-      expect(calc).to.exist();
-    });
-
     it('should exist the method `sum`', () => {
-      expect(calc.sum).to.exist();
-      expect(calc.sum).to.be.a('function');
+      expect(sum).to.exist();
+      expect(sum).to.be.a('function');
     });
 
     it('should exist the method .sub`', () => {
-      expect(calc.sub).to.exist();
-      expect(calc.sub).to.be.a('function');
+      expect(sub).to.exist();
+      expect(sub).to.be.a('function');
     });
 
     it('should exist the method `mult`', () => {
-      expect(calc.mult).to.exist();
-      expect(calc.mult).to.be.a('function');
+      expect(mult).to.exist();
+      expect(mult).to.be.a('function');
     });
 
     it('should exist the method `div`', () => {
-      expect(calc.div).to.exist();
-      expect(calc.div).to.be.a('function');
+      expect(div).to.exist();
+      expect(div).to.be.a('function');
     });
   });
 
   describe('Sum', () => {
-    it('should return 4 when `calc.sum(2, 2)`', () => {
-      expect(calc.sum(2, 2)).to.be.equal(4);
+    it('should return 4 when `sum(2, 2)`', () => {
+      expect(sum(2, 2)).to.be.equal(4);
     });
   });
 
   describe('Sub', () => {
-    it('should return 2 when `calc.sub(7, 5)`', () => {
-      expect(calc.sub(7, 5)).to.be.equal(2);
+    it('should return 2 when `sub(7, 5)`', () => {
+      expect(sub(7, 5)).to.be.equal(2);
     });
   });
 
   describe('Mult', () => {
-    it('should return 6 when `calc.mult(2, 3)`', () => {
-      expect(calc.mult(2, 3)).to.be.equal(6);
+    it('should return 6 when `mult(2, 3)`', () => {
+      expect(mult(2, 3)).to.be.equal(6);
     });
   });
 
   describe('Div', () => {
     it('should return 5 when `calc.div(25, 5)`', () => {
-      expect(calc.div(25, 5)).to.be.equal(5);
+      expect(div(25, 5)).to.be.equal(5);
     });
 
     it('should return `no possible, asshole!` when divide by 0', () => {
-      expect(calc.div(4, 0)).to.be.equal('no possible, asshole!');
+      expect(div(4, 0)).to.be.equal('no possible, asshole!');
     });
   });
 });
